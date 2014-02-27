8010fbaa72361fb013533e3edf257d7f4b36b11c
Pabel Brown <david.pabel.brown@gmail.com>
2014-02-27 17:38:59 -0500

    First Commit - Entire Director

---
+++ tango_with_django_web_project/tango_with_django_web_project/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_web_project.views.home', name='home'),
+    # url(r'^tango_with_django_web_project/', include('tango_with_django_web_project.tango_with_django_web_project.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
